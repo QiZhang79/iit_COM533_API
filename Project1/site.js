e934f95a48b26ef8f48feb4b115e5e41c16bf0b6
Qi Zhang <zhangqitydx@gmail.com>
2017-09-13 14:22:10 -0500

    Use JSON Data to design a simple project; jQuery

---
+++ Project1/site.js
@@ -0,0 +1,13 @@
+var user = {
+  "name": "QiZhang79",
+  "id": "001",
+  "avatar_url": "https://avatars2.githubusercontent.com/u/21212693?v=4",
+  "location": "Chicago",
+  "created_at": "2016-08-24T05:08:13Z"
+}
+
+$('#content').append('<img src="' + user["avatar_url"] + '" />');
+$('#name').append('<a href="https://github.com/'+user["name"]+'">' + user["name"] + '</a>');
+$('#id').append(user["id"]);
+$('#location').append(user["location"]);
+$('#start').append(user["created_at"]);
